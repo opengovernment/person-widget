a2361ee313e42f3b3446fa4baf3c0bbd4e072c48
Walter McGinnis <wm@waltermcginnis.com>
2015-03-14 10:15:25 +1300

    Add ability to pass in full details for person
    * now no need for ajax call in this case

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var person_id = this.controllerFor('application').get('attrs.person.id');
-    return this.apijax.GET('/identifier.json', { 'person_id': person_id });
+    var person = this.controllerFor('application').get('attrs.person'),
+        person_id = person.id,
+        model;
+
+    if (!Ember.isEmpty(person.full_name)) {
+      // we have full passed in details for the person, no need for lookup
+      model = [person];
+    } else {
+      model = this.apijax.GET('/identifier.json', { 'person_id': person_id });
+    }
+
+    return model;
   },
   setupController: function(controller, model) {
     this._super(controller, model);
