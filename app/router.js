16776e896efe8f3cee0565f750a13b3fc4e801f8
Walter McGinnis <wm@waltermcginnis.com>
2015-04-10 16:28:35 +1200

    Update to ember-cli 0.2.3 & ember 1.11.1

--- app/router.js
+++ app/router.js
@@ -5,12 +5,10 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
   this.route('questions', function() {
     this.route('new');
   });
 
   this.route('thanks');
 });
-
-export default Router;
